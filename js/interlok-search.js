6ac343d2b8fae3e89a2f35d9f26f09cf7dee46a2
Sebastien Belin <sebastien.belin@proagrica.com>
2023-08-01 16:05:34 +0900

    Support listView param in web component

--- js/interlok-search.js
+++ js/interlok-search.js
@@ -12,10 +12,11 @@ class InterlokSearch extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'open' });
 
     const page = this.getAttribute("page") == "optional-component-search" ? this.getAttribute("page")  : "component-search";
+    const listViewParam = this.hasAttribute("listView") ? "?listView=" + (this.getAttribute("listView") == "true") : "";
 
     div.innerHTML = /*html*/`
     <iframe class="interlok-search-iframe"
-            src="https://interlok.adaptris.net/interlok-component-search/index-embeddable.html#/${page}"
+            src="https://interlok.adaptris.net/interlok-component-search/index-embeddable.html#/${page}${listViewParam}"
             frameBorder="0"
             style="position: absolute; top: 0; bottom: 0; left: 0; right: 0; height: 100%; width: 100%;"/>
     `;
